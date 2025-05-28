54d6a75c98b792ea2d28769e9cc439d39f69a381
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-05-28 16:27:06 -0300

    Criptografar e decriptografar mensagem

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ import random
 import math
 import string 
 
-chars = string.punctuation + string.digits + string.ascii_letters
+chars = " " + string.punctuation + string.digits + string.ascii_letters + "áàâãäéèêëíìîïóòôõöúùûüçÁÀÂÃÄÉÈÊËÍÌÎÏÓÒÔÕÖÚÙÛÜÇ"
+chars = list(chars)
+key = chars.copy()
 
-print(chars)
+random.shuffle(key)
+
+print(f"Chars: {chars}")
+print(f"Key: {key}")
+
+#ENCRYPTION 
+plain_text = input("Enter the text to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    if letter in chars:
+        index = chars.index(letter)
+        cipher_text += key[index]
+    else:
+        print(f"Warning: Character '{letter}' not in chars list, skipping.")
+        # Optionally: cipher_text += letter  # or another placeholder
+
+print(f"Original menssage: {plain_text}")
+print(f"encrypted message: {cipher_text}")
+
+#EDECRYPTION
+cipher_text = input("Enter the text to decrypt: ")
+plain_text = ""
+
+for letter in plain_text:
+        index = chars.index(letter)
+        cipher_text += chars[index]
+
+
+print(f"Original menssage: {cipher_text}")
+print(f"encrypted message: {plain_text}")
+
